9bcc8fb7edd8ed8f666f2f02b71f2c1ff1ac0f80
Johann Hagler <johann.hagler@gmail.com>
2023-11-05 16:21:08 +0100

    added contitionals

---
+++ Lesson 2/Lesson 2.md
@@ -0,0 +1,8 @@
+# Python Introduction
+2023-11-05
+
+## Control structures
+## Conditional statements: if, else, elif
+
+A condition is true or false.
+
---
+++ Lesson 2/lesson2.py
@@ -0,0 +1,9 @@
+number1 = input("Enter a number: ")
+number2 = input("Enter a number: ")
+
+if number1 > number2:
+    print(number1, "is greater than", number2)
+elif number1 < number2:
+    print(number1, "is less than", number2)
+else:
+    print(number1, "is equal to", number2)
