35c20fc3f9c9301debc0600ae546e32445e45e59
Johann Hagler <johann.hagler@gmail.com>
2023-12-17 17:22:02 +0100

    update

--- Lesson 5/README.md
+++ Lesson 5/README.md
@@ -1,8 +1,22 @@
 # Python Programming 5
 2023-12-17
 
-recap: the 5 elements of programming
-create a game of rock, paper, scissors
-refactoring
-clean code
-DRY principle
+## Data Structure
+### Array
+['rock', 'paper', 'scissors']
+
+## Branching, if conditions
+### Locical Operators
+- `or` operator: `True` if either of the operands is `True`
+- `and` operator: `True` if both of the operands are `True`
+
+## Software Development Principles
+Clean Code is code that is easy to understand and easy to change for a human.
+
+### A Function should do only one thing
+If we have code that does not follow good software development principles, we can refactor it.
+Refactoring means changing the code without changing the functionality of the code.
+
+Rule: A function should not have more than 10 lines of code.
+
+If it is longer, that is what we call a Code Smell.
---
+++ Lesson 5/game.py
@@ -0,0 +1,43 @@
+# Interaction with computer: command line
+# Input / output system: command line
+
+import random
+
+# define global array of options (element 0: 'rock', element 1: 'paper', element 2: 'scissors')
+# arrays have elements of the same type
+options = ['rock', 'paper', 'scissors']
+
+def GetUserChoice():
+    user_choice = input('Enter your choice (rock, paper, scissors, q - quit): ')
+    return user_choice
+
+def GetComputerChoice():
+    computer_choice = random.choice(options)
+    return computer_choice
+
+def CalculateWinner(user_choice, computer_choice):
+    print('Computer chose: ', computer_choice)
+    if user_choice == computer_choice:
+        print('It\'s a tie!')
+    # operand1 or operand2 or operand3
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'scissors' and computer_choice == 'paper') or \
+         (user_choice == 'paper' and computer_choice == 'rock'):
+        print('You win!')
+    else:
+        print('You lose!')
+
+def StartGame():
+    user_choice = GetUserChoice()
+    if user_choice == 'q':
+        exit()
+
+    if user_choice not in options:
+        print('Invalid choice. Please choose rock, paper or scissors.')
+        return
+
+    computer_choice = GetComputerChoice()
+    CalculateWinner(user_choice, computer_choice)
+
+while True:
+    StartGame()
